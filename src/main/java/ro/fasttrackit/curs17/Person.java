f37121d1bf42d83aa48cff3db3307d2efbbad023
Iaro <iaroslav.slovak@gmail.com>
2021-06-13 09:58:27 +0300

    Initial commit

--- src/main/java/ro/fasttrackit/curs17/Person.java
+++ src/main/java/ro/fasttrackit/curs17/Person.java
@@ -44,8 +44,7 @@ public class Person {
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (!(o instanceof Person)) return false;
-        Person person = (Person) o;
+        if (!(o instanceof Person person)) return false;
         return getAge() == person.getAge() && Objects.equals(getFirstName(), person.getFirstName()) && Objects.equals(getLastName(), person.getLastName()) && Objects.equals(getCity(), person.getCity());
     }
 
--- src/test/java/ro/fasttrackit/curs17/PersonServiceTest.java
+++ src/test/java/ro/fasttrackit/curs17/PersonServiceTest.java
@@ -10,7 +10,55 @@ import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 class PersonServiceTest {
 
     @Test
-    @DisplayName("WHEN majorPerson() THEN return all the major persons")
+    @DisplayName("WHEN getFullName() is used THEN return first and last name of persons")
+    void fullname() {
+        //SETUP
+        PersonService service = populateService();
+        //RUN
+        List<String> result = service.getFullName();
+        //ASSERT
+        assertThat(result).isEqualTo(List.of(
+                "Popescu Andrei",
+                "Bitman Dan",
+                "Popa Adrian",
+                "Darie Emanuel",
+                "Radulescu Nicoleta"
+        ));
+    }
+
+    @Test
+    @DisplayName("WHEN fromCity() is used THEN return persons from Oradea")
+    void fromOradea() {
+        //SETUP
+        PersonService service = populateService();
+        //RUN
+        List<Person> result = service.fromCity();
+        //ASSERT
+        assertThat(result).hasSize(2);
+        assertThat(result).isEqualTo(List.of(
+                new Person("Popescu", "Andrei", 16, "Oradea"),
+                new Person("Bitman", "Dan", 50, "Oradea")
+        ));
+    }
+
+    @Test
+    @DisplayName("WHEN fromCities() is used THEN return persons from Oradea and Cluj")
+    void fromOradeaAndCluj() {
+        //SETUP
+        PersonService service = populateService();
+        //RUN
+        List<Person> result = service.fromCities();
+        //ASSERT
+        assertThat(result).hasSize(3);
+        assertThat(result).isEqualTo(List.of(
+                new Person("Popescu", "Andrei", 16, "Oradea"),
+                new Person("Bitman", "Dan", 50, "Oradea"),
+                new Person("Popa", "Adrian", 43, "Cluj")
+        ));
+    }
+
+    @Test
+    @DisplayName("WHEN majorPerson() is used THEN return all the major persons")
     void major() {
         //SETUP
         PersonService service = populateService();
@@ -21,8 +69,48 @@ class PersonServiceTest {
         assertThat(result).isEqualTo(List.of(
                 new Person("Bitman", "Dan", 50, "Oradea"),
                 new Person("Popa", "Adrian", 43, "Cluj"),
-                new Person("Radulescu", "Nicoleta", 75, "Oradea")
+                new Person("Radulescu", "Nicoleta", 75, "Iasi")
         ));
+    }
+
+    @Test
+    @DisplayName("WHEN sortByFirstName() is used THEN return list of persons sorted by first name")
+    void sortFirstName() {
+        //SETUP
+        PersonService service = new PersonService(List.of(
+                new Person("Popescu", "Andrei", 16, "Oradea"),
+                new Person("Bitman", "Dan", 50, "Oradea"),
+                new Person("Radulescu", "Nicoleta", 75, "Iasi")
+        ));
+        //RUN
+        List<Person> result = service.sortByFirstName();
+        //ASSERT
+        assertThat(result).isEqualTo(List.of(
+                new Person("Bitman", "Dan", 50, "Oradea"),
+                new Person("Popescu", "Andrei", 16, "Oradea"),
+                new Person("Radulescu", "Nicoleta", 75, "Iasi")
+        ));
+
+    }
+
+    @Test
+    @DisplayName("WHEN sortByLastName() is used THEN return list of persons sorted by last name")
+    void sortLastName() {
+        //SETUP
+        PersonService service = new PersonService(List.of(
+                new Person("Bitman", "Dan", 50, "Oradea"),
+                new Person("Radulescu", "Nicoleta", 75, "Iasi"),
+                new Person("Popescu", "Andrei", 16, "Oradea")
+        ));
+        //RUN
+        List<Person> result = service.sortByLastName();
+        //ASSERT
+        assertThat(result).isEqualTo(List.of(
+                new Person("Popescu", "Andrei", 16, "Oradea"),
+                new Person("Bitman", "Dan", 50, "Oradea"),
+                new Person("Radulescu", "Nicoleta", 75, "Iasi")
+        ));
+
     }
 
     private PersonService populateService() {
@@ -30,8 +118,8 @@ class PersonServiceTest {
                 new Person("Popescu", "Andrei", 16, "Oradea"),
                 new Person("Bitman", "Dan", 50, "Oradea"),
                 new Person("Popa", "Adrian", 43, "Cluj"),
-                new Person("Darie", "Emanuel", 10, "Cluj"),
-                new Person("Radulescu", "Nicoleta", 75, "Oradea")
+                new Person("Darie", "Emanuel", 10, "Brasov"),
+                new Person("Radulescu", "Nicoleta", 75, "Iasi")
         ));
     }
 }
