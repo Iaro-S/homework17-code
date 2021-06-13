4df497143278254c2d96aaf7b9187c913c1ccee3
Iaro <iaroslav.slovak@gmail.com>
2021-06-13 12:28:15 +0300

    final commit

--- src/main/java/ro/fasttrackit/curs17/PersonService.java
+++ src/main/java/ro/fasttrackit/curs17/PersonService.java
@@ -40,6 +40,37 @@ public class PersonService {
                 .collect(toList());
     }
 
+    public List<String> firstNamesCapitalized() {
+        return persons.stream()
+                .map(person -> person.getFirstName().toUpperCase())
+                .collect(toList());
+    }
+
+    public List<String> lastNameAbbreviated() {
+        return persons.stream()
+                .map(person -> person.getFirstName() + " " + person.getLastName().charAt(0) + ".")
+                .collect(toList());
+    }
+
+    public List<Person> ageBetween() {
+        return persons.stream()
+                .filter(person -> person.getAge() > 18)
+                .filter(person -> person.getAge() < 60)
+                .collect(toList());
+    }
+
+    public List<Person> firstNameWithA() {
+        return persons.stream()
+                .filter(person -> person.getFirstName().startsWith("A"))
+                .collect(toList());
+    }
+
+    public List<String> uniqueFirstName() {
+        return persons.stream()
+                .map(Person::getFirstName).distinct()
+                .collect(toList());
+    }
+
     public List<Person> sortByFirstName() {
         return persons.stream()
                 .sorted(Comparator.comparing(Person::getFirstName))
@@ -49,6 +80,14 @@ public class PersonService {
     public List<Person> sortByLastName() {
         return persons.stream()
                 .sorted(Comparator.comparing(Person::getLastName))
+                .collect(toList());
+    }
+
+    public List<Person> multiCriteriaSort() {
+        return persons.stream()
+                .sorted(Comparator.comparing(Person::getFirstName)
+                        .thenComparing(Comparator.comparing(Person::getLastName)
+                                .thenComparing(Person::getAge)))
                 .collect(toList());
     }
 }
